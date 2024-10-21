4c6866dd9ab2272428f693a7b8ef004f51ce0b2d
DQE92 <duquedylan@icloud.com>
2024-10-21 18:16:04 +0200

    add backslash

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)/*affiche le message*/
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
