2c0181fbd539263b635c8d2018f49113bfd2b94e
DQE92 <duquedylan@icloud.com>
2024-10-22 11:29:52 +0200

    1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,18 @@
-#include <stdio.h> /* bibliothèque standard */
-int main(void) /* point d'entrée */
+/*
+ * File: 5-printf.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," .
+ *
+ * Return: stope 0.
+ */
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
