f0765a2f4deee1ae599ada30eca1ef72d14d6bb6
DQE92 <duquedylan@icloud.com>
2024-10-24 10:56:14 +0200

    sans espaces

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i;
 
-for (i = 1; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
-printf("%d ", i);
+putchar(i + '0');
 }
-printf("\n");
+putchar('\n');
 
 return (0);
-
 }
