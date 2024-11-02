6d5d26cce48181faacb01218e77238a365818d4f
DQE92 <duquedylan@icloud.com>
2024-11-02 13:06:16 +0100

    4

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strcat - Concatène deux chaînes de caractères.
@@ -13,13 +12,11 @@ char *_strcat(char *dest, char *src)
     int i = 0;
     int j = 0;
 
-    // Trouve la fin de dest
     while (dest[i] != '\0')
     {
         i++;
     }
 
-    // Ajoute src à la fin de dest
     while (src[j] != '\0')
     {
         dest[i] = src[j];
@@ -27,7 +24,6 @@ char *_strcat(char *dest, char *src)
         j++;
     }
 
-    // Ajoute le caractère nul à la fin de la chaîne résultante
     dest[i] = '\0';
 
     return dest;
