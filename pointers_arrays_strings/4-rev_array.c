be51858f124c18cd91de8752ec7069842fc11657
DQE92 <duquedylan@icloud.com>
2024-11-02 23:37:48 +0100

    5

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,18 +2,18 @@
 
 void reverse_array(int *a, int n)
 {
-    int i = 0;       // Début du tableau
-    int j = n - 1;   // Fin du tableau
-    int temp;        // Variable temporaire pour l'échange
+    int i = 0;       
+    int j = n - 1;   
+    int temp;        
 
     while (i < j)
     {
-        // Échange a[i] et a[j]
-        temp = a[i];  // Stocke temporairement la valeur de a[i]
-        a[i] = a[j];  // Place la valeur de a[j] dans a[i]
-        a[j] = temp;  // Place la valeur temporaire (ancienne a[i]) dans a[j]
+        
+        temp = a[i];  
+        a[i] = a[j];  
+        a[j] = temp;  
 
-        // Avance l'indice i vers le centre et recule j vers le centre
+       
         i++;
         j--;
     }
