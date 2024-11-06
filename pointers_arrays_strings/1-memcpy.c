ce39a1e9cb456be39fb9cfe4828f20613fae69f9
DQE92 <duquedylan@icloud.com>
2024-11-06 14:36:14 +0100

    2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * _memcpy - Copies a specified number of bytes from source to destination
+ * dest: The destination buffer where bytes will be copied
+ * src: The source buffer from which bytes are copied
+ * n: The number of bytes to copy
+ *
+ * Return: A pointer to the destination buffer
+ */
 char *_memcpy(char *dest, char *src, unsigned int n) 
 {
     unsigned int i; 
