ea50ae05572432e7f5a5bffb8d1ff0c340189646
DQE92 <duquedylan@icloud.com>
2024-11-08 14:55:57 +0100

    tell the name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the program name.
+ * argc: Number of command-line arguments (unused in this program).
+ * argv: Array of command-line arguments, with argv[0] being the program's name.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
