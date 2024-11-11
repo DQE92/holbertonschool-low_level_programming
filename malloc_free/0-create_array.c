9d7ca3998c01b92ceeec0c37e3b6eb632cf53e2d
DQE92 <duquedylan@icloud.com>
2024-11-11 15:19:17 +0100

    malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/*1- variables
+*2- verifie si la taille est 0 donc return null
+*3- alloue la mémoire
+*4- initialise chaque élément avec 'c'
+*5- retourne le pointeur vers tableay
+*/
+char *create_array(unsigned int size, char c) 
+{
+char *array;
+unsigned int i;
+
+if (size == 0) 
+{
+return NULL;
+}
+
+array = (char *)malloc(size * sizeof(char));
+if (array == NULL) {
+return NULL;
+}
+
+for (i = 0; i < size; i++) 
+{
+array[i] = c;
+}
+
+return array;
+}
