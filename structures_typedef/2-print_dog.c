3db92419edd2982608ed02d8409b40aebfe73e46
DQE92 <duquedylan@icloud.com>
2024-11-15 00:00:18 +0100

    printf

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+#include <string.h>
+/*
+*return - si d est null, rien ne se passe
+*if - verifier et afficher le nom
+*printf - afficher l'âge sans besoin de vérifier NULL, car c'est un float (pas un pointeur)
+*if - verifier et afficher le propriétaire
+*/
+void print_dog(struct dog *d)
+{
+
+    if (d == NULL) 
+    {
+        return;
+    }
+
+    if (d->name == NULL) 
+    {
+        printf("Name: (nil)\n");
+    } else 
+    {
+        printf("Name: %s\n", d->name);
+    }
+
+    printf("Age: %f\n", d->age);
+
+    if (d->owner == NULL) 
+    {
+        printf("Owner: (nil)\n");
+    } else 
+    {
+        printf("Owner: %s\n", d->owner);
+    }
+}
